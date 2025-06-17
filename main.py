63c9e3500caea57a560aafb2e2097e1f70533d12
Pasan Kalhara <76808092+Kalharapasan@users.noreply.github.com>
2025-06-17 23:20:55 +0530

    Update The Project

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
 """
-Network Analyzer - A Wireshark-like packet capture and analysis tool
-Enhanced version with better Windows support and alternative capture methods
 
 Requirements:
 - pip install scapy psutil
